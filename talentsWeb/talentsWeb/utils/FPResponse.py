ab9df53486d4038bcc2730cf9d5b21d5a9290de4
fujie <fujie@xiyoulinux.org>
2020-05-16 13:13:58 +0800

    修改响应content-type为application/json

--- talentsWeb/talentsWeb/utils/FPResponse.py
+++ talentsWeb/talentsWeb/utils/FPResponse.py
@@ -6,8 +6,10 @@ from django.core.serializers.json import DjangoJSONEncoder
 class FPResponse:
     @classmethod
     def success(cls, data):
-        return HttpResponse(json.dumps({"succeed": True, "data": data}, sort_keys=True, cls=DjangoJSONEncoder))
+        return HttpResponse(json.dumps({"succeed": True, "data": data}, sort_keys=True, cls=DjangoJSONEncoder),
+                            content_type="application/json")
 
     @classmethod
     def failure(cls, message=None):
-        return HttpResponse(json.dumps({"succeed": False, "message": message}, sort_keys=True, cls=DjangoJSONEncoder))
+        return HttpResponse(json.dumps({"succeed": False, "message": message}, sort_keys=True, cls=DjangoJSONEncoder),
+                            content_type="application/json")
